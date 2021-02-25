864b7dac8ce983e1454a32ea23bd7816d32d3792
marcosanf <marcosanf.c@gmail.com>
2021-02-25 13:17:35 -0400

    reusable widget custom text

---
+++ lib/src/widgets/custom_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'file:///C:/Users/Usuario/IdeaProjects/foodelivery/lib/src/commons.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double size;
+  final Color colors;
+  final FontWeight weight;
+
+  const CustomText({Key key, @required this.text, this.size, this.colors, this.weight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: size ?? 16.0,
+        fontWeight: weight ?? FontWeight.normal,
+        color: colors ?? black
+      ),
+    );
+  }
+}
