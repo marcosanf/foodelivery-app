a726f0f52279747a8a0921e6d62e193b3414b559
marcosanf <marcosanf.c@gmail.com>
2021-02-26 17:17:33 -0400

    creating a reusable icon button

---
+++ lib/src/widgets/small_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:foodelivery/src/commons.dart';
+
+class SmallButton extends StatelessWidget {
+
+  final IconData icon;
+
+  const SmallButton({Key key, this.icon}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          color: red
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Icon(icon, size: 16.0, color: white),
+      ),
+    );
+  }
+}
