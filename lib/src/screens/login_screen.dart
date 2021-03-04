b1ff369a43ffc7c880ff6f55dd9faf2fef0aa330
marcosanf <marcosanf.c@gmail.com>
2021-03-04 18:02:19 -0400

    creating the login screen

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:foodelivery/src/helpers/commons.dart';
+import 'package:foodelivery/src/helpers/screen_navigation.dart';
+import 'package:foodelivery/src/screens/home_screen.dart';
+import 'package:foodelivery/src/screens/registration.dart';
+import 'package:foodelivery/src/widgets/custom_text.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/logo.png', width: 240.0, height: 240.0,),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: grey),
+                  borderRadius: BorderRadius.circular(15.0)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'E-mail',
+                      border: InputBorder.none,
+                      icon: Icon(Icons.email)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15.0)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Password',
+                        border: InputBorder.none,
+                        icon: Icon(Icons.lock)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: GestureDetector(
+                onTap: () {
+                  changeScreen(context, HomeScreen());
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: red,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(text: "Login", colors: white, size: 22.0,),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                changeScreen(context, RegistrationScreen());
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomText(text: 'Register here', size: 20.0,),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
