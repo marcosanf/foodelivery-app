bee92b9f38dde1a4d5ecc8917dfa0a70123fc52d
marcosanf <marcosanf.c@gmail.com>
2021-03-04 13:18:49 -0400

    incrementing the onTap function to bottom navigation

--- lib/src/widgets/bottom_navigation_button.dart
+++ lib/src/widgets/bottom_navigation_button.dart
@@ -5,23 +5,28 @@ class ButtonNavigation extends StatelessWidget {
 
   final String image;
   final String name;
+  final Function onTap;
 
-  const ButtonNavigation({Key key, this.image, this.name}) : super(key: key);
+
+  const ButtonNavigation({Key key, this.image, this.name, this.onTap}) : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 2.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            'images/$image',
-            width: 26,
-          ),
-          CustomText(text: name, size: 12.0,)
-        ],
+      child: GestureDetector(
+        onTap: onTap ?? null,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'images/$image',
+              width: 26,
+            ),
+            CustomText(text: name, size: 12.0,)
+          ],
+        ),
       ),
     );
   }
